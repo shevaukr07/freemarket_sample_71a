8260b4ecbd5a63284e3411ac57fee9e799ea0b20
nagao <38156084+yasanori@users.noreply.github.com>
2020-03-20 14:11:39 +0900

    seacret

--- db/migrate/20200320034142_create_addresses.rb
+++ db/migrate/20200320034142_create_addresses.rb
@@ -3,6 +3,11 @@ class CreateAddresses < ActiveRecord::Migration[5.0]
     create_table :addresses do |t|
       t.references :user
       t.string :postal_code, null: false, :limit => 7
+      t.integer :prefecture, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :apartment
+      t.integer :phone_num
       t.timestamps
     end
   end
