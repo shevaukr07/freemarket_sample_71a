1d71fc631a3a78a868dc799c7845b1a661e1928a
shevaukr07 <61219381+shevaukr07@users.noreply.github.com>
2020-03-25 15:46:15 +0900

    commit

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
+  belongs_to_active_hash :prefecture
   validates :postal_code, :prefecture, :city, :address, presence: true
 
 end
