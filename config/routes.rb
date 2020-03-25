295c7a9a51840bba7d4d673dc3f0d9364b021513
takayak <tktknet0530@gmail.com>
2020-03-26 00:02:50 +0900

    payjp
    ローカル環境で動くPay.jp完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+
+  get 'purchase/done'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users, controllers: {
@@ -18,8 +22,21 @@ Rails.application.routes.draw do
       post 'purchase'
     end
   end
-  resources :cards
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
+  end
   # get 'items/purchase' :to 'items#purchase'
 
     
