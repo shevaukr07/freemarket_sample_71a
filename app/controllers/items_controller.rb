85412ceb0a819d685c513bddd3097945dbc98b1e
nagao <38156084+yasanori@users.noreply.github.com>
2020-03-27 12:41:57 +0900

    code-review edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,29 +24,12 @@ class ItemsController < ApplicationController
     redirect_to root_path and return
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @new_item = Item.new(item_update_params)
-    unless @new_item.valid?
-      redirect_to edit_item_path and return
-    end
-    @item.update(item_update_params)
-    redirect_to root_path and return
-  end
-
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :introduce, :brand_id, :category_id, :size_id, :commodity_condition_id, :shipping_charge_id, :shipping_method_id,:prefecture_id, :shipping_day_id, item_images_attributes: [:image]).merge(seller_id: current_user.id).merge(buyer_id: current_user.id)
   end
 
-  def item_update_params
-    params.require(:item).permit(:name, :price, :introduce, :brand_id, :size_id, :category_id, :commodity_condition_id, :shipping_charge_id, :shipping_method_id,:prefecture_id, :shipping_day_id, item_images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id).merge(buyer_id: current_user.id)
-  end
 
   def purchase
     Payjp::Charge.create(
