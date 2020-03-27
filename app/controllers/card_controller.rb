baeaf81a9b9c2166ac312b69171e1ba6fde0bd1b
takayak <tktknet0530@gmail.com>
2020-03-27 21:34:30 +0900

    binding remove
    binding

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -18,7 +18,6 @@ class CardController < ApplicationController
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      # binding.pry
       if @card.save
         redirect_to action: "show"
       else
