499b7d27142ed2ad83920be9a04b12861d8e7f54
takayak <tktknet0530@gmail.com>
2020-03-28 18:17:47 +0900

    pay.jp
    pay.jp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
@@ -15,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   def production?
     Rails.env.production?
+    
   end
 
   def basic_auth
