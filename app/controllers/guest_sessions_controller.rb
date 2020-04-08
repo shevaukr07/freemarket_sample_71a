de202c3ee070ef33ae6520abd937b0477920f3ee
takayak <tktknet0530@gmail.com>
2020-04-08 13:39:26 +0900

    Update guest_sessions_controller.rb

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -9,7 +9,7 @@ class GuestSessionsController < ApplicationController
   def create2
     @user = User.find_by(email: 'test@seller')
     sign_in(:user, @user)
-    flash[:notice] = 'ゲスト出品ユーザーでログインしました'
+    flash[:notice] = 'ゲスト出品ユーザーでログインしました!'
     redirect_to root_path
   end
 end
