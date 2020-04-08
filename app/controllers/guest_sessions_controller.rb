9ea80dcdfcc67ae32daf97ccd34dfc05d8b31b6c
takayak <tktknet0530@gmail.com>
2020-04-08 13:29:16 +0900

    ゲスト購入機能
    ゲスト購入機能
    ゲスト出品機能

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,13 +1,13 @@
 class GuestSessionsController < ApplicationController
   def create
-    @user = User.find_by(email: 'test@buy')
+    @user = User.find_by(email: 'test@buyer')
     sign_in(:user, @user)
     flash[:notice] = 'ゲスト購入ユーザーでログインしました'
     redirect_to root_path
   end
 
   def create2
-    @user = User.find_by(email: 'test@sell')
+    @user = User.find_by(email: 'test@seller')
     sign_in(:user, @user)
     flash[:notice] = 'ゲスト出品ユーザーでログインしました'
     redirect_to root_path
