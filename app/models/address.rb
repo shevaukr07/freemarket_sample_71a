19f3079cf36eaaf0569fafc95d52be7e7fa533fa
nagao <38156084+yasanori@users.noreply.github.com>
2020-04-09 18:38:09 +0900

    バリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user, optional: true
   belongs_to_active_hash :prefecture
-  validates :postal_code, :prefecture, :city, :address, presence: true
+  validates :name, :name_kana, :postal_code, :prefecture, :city, :address, presence: true
 
 end
